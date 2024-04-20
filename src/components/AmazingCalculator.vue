ea9cb01d38c061c4b41c7423fb2f72d0bc37fdcc
dinhgiatoanvn <dinhgiatoanvn@gmail.com>
2024-04-20 08:48:08 +0700

    Calculator new version.

--- src/components/AmazingCalculator.vue
+++ src/components/AmazingCalculator.vue
@@ -1,102 +1,141 @@
 <script setup>
-import {ref, computed} from 'vue'
+  import { ref } from "vue";
 
-const display = ref("0")
-const appendToDisplay = (value) => {
-    if (display.value === '0' && value !== '.') {
-        display.value = value
-    } else {
-        display.value += value
-    }
-}
-const calculate = () => {
-    try {
-        display.value = eval(display.value).toString()
-    } catch (error) {
-        display.value = '0'
-        errorCal = true
-    }
-}
-const displayClass = computed(() => {
-    return display.value.length > 12 ? 'small-text' : ''
-})
-const clearDisplay = () => {
-    display.value = '0'
-}
+  const output = ref('');
+  const previousValue = ref(null);
+  const operationFired = ref(false);
+  const countGetNumber = ref(0);
+  let operation;
+      
+  const clearField = () => {
+    output.value = '';
+  };
+  const setNagativeOrPositive = () => {
+    output.value = output.value[0] === '-' ? output.value.slice(1) : `-${output.value}`;
+  };
+  const calculatePercentage = () => {
 
+    if (output.value) {
+      output.value = parseFloat(output.value)/100;
+    }
+  };
+  const getNumber = (number) => {
+
+    if(operationFired.value === true) {
+      output.value = '';
+      operationFired.value = false;
+    }
+
+    output.value = `${output.value}${number}`;
+    countGetNumber.value += 1;
+  };
+  const getDot = () => {
+
+    if (output.value === '') {
+      output.value = '0.';
+    } else if (output.value === '-') {
+      output.value = '-0.';
+    } else if (output.value.indexOf('.') === -1) {
+      output.value = output.value + '.';
+    }
+    // Hàm indexOf('.') sẽ trả về chỉ số đầu tiên của dấu chấm nếu nó được tìm thấy trong chuỗi. Nếu không tìm thấy, hàm này sẽ trả về -1.
+  };
+  const processOutput = (string) => {
+
+    if (['add', 'subtract', 'divide', 'multiply'].includes(string)) {
+      if (string == 'add') {
+        operation = (a, b) => output.value = parseFloat(a) + parseFloat(b);
+      } else if (string == 'subtract') {
+        operation = (a, b) => output.value = parseFloat(a) - parseFloat(b);
+      } else if (string == 'divide') {
+        operation = (a, b) => output.value = parseFloat(a) / parseFloat(b);
+      } else if (string == 'multiply') {
+        operation = (a, b) => output.value = parseFloat(a) * parseFloat(b);
+      }
+    }
+
+    previousValue.value = output.value;
+    operationFired.value = true;
+  };
+  const updateOutput = () => {
+    if (countGetNumber.value > 1) {
+      if (previousValue.value && output.value) {
+        output.value = `${operation(previousValue.value, output.value)}`;
+        countGetNumber.value = 0;
+      }
+    }
+  };
+  const deleteNumber = () => {
+
+    if (output.value.length) {
+      output.value = output.value.slice(0, -1);
+    }
+  };
 </script>
 
 <template>
-    <div class="">
-        <div class="calculator">
-            <input v-model="display" :class="displayClass" readonly>
-            <div class="buttons">
-                <button @click="appendToDisplay('7')">7</button>
-                <button @click="appendToDisplay('8')">8</button>
-                <button @click="appendToDisplay('9')">9</button>
-                <button @click="appendToDisplay('/')">/</button>
-
-                <button @click="appendToDisplay('4')">4</button>
-                <button @click="appendToDisplay('5')">5</button>
-                <button @click="appendToDisplay('6')">6</button>
-                <button @click="appendToDisplay('*')">*</button>
-
-                <button @click="appendToDisplay('1')">1</button>
-                <button @click="appendToDisplay('2')">2</button>
-                <button @click="appendToDisplay('3')">3</button>
-                <button @click="appendToDisplay('-')">-</button>
-
-                <button @click="appendToDisplay('0')">0</button>
-                <button @click="appendToDisplay('.')">.</button>
-                <button @click="calculate('=')">=</button>
-                <button @click="appendToDisplay('+')">+</button>
-            </div>
-            <button @click="clearDisplay" class="clear-button">C</button>
-        </div>
+  <div>
+    <h1 class="m-3">Calculator</h1>
+    <div class="col-md-4 m-3">
+      <table class="table">
+        <tbody>
+          <tr class="output">
+            <td colspan="4">{{ output || 0 }}</td>
+          </tr>
+          <tr>
+            <td @click="clearField">C</td>
+            <td @click="setNagativeOrPositive">+/-</td>
+            <td @click="calculatePercentage">%</td>
+            <td @click="processOutput('divide')" class="lastColumn">/</td>
+          </tr>
+          <tr>
+            <td @click="getNumber('7')">7</td>
+            <td @click="getNumber('8')">8</td>
+            <td @click="getNumber('9')">9</td>
+            <td @click="processOutput('multiply')" class="lastColumn">x</td>
+          </tr>
+          <tr>
+            <td @click="getNumber('4')">4</td>
+            <td @click="getNumber('5')">5</td>
+            <td @click="getNumber('6')">6</td>
+            <td @click="processOutput('subtract')" class="lastColumn">-</td>
+          </tr>
+          <tr>
+            <td @click="getNumber('1')">1</td>
+            <td @click="getNumber('2')">2</td>
+            <td @click="getNumber('3')">3</td>
+            <td @click="processOutput('add')" class="lastColumn">+</td>
+          </tr>
+          <tr>
+            <td @click="getNumber('0')">0</td>
+            <td @click="getDot()">.</td>
+            <td @click="deleteNumber">&#8592;</td>
+            <td @click="updateOutput" class="lastColumn">=</td>
+          </tr>
+        </tbody>
+      </table>
     </div>
+  </div>
 </template>
 
 <style scoped>
-input {
-  padding: 10px 20px;
-  margin-bottom: 20px;
+.output td {
+  background: #000;
+  color: #FFF;
 }
-
-.calculator {
-  max-width: 300px;
-  margin: 0 auto;
-  padding: 20px;
-  border: 1px solid #ccc;
-  border-radius: 5px;
-  margin-top: 30px;
+table tr td {
+  text-align: center;
+  border: 1px solid #e4e4e4;
 }
-
-.display {
-  width: 100%;
-  margin-bottom: 10px;
-  padding: 10px;
-  font-size: 18px;
-  text-align: right;
+.lastColumn {
+  background: orange;
+  color: #FFF;
 }
-
-.buttons {
-  display: grid;
-  grid-template-columns: repeat(4, 1fr);
-  gap: 10px;
+table tr td:active {
+  background: #000;
+  color: #FFF;
 }
-
-button {
-  width: 100%;
-  padding: 10px;
-  font-size: 18px;
-}
-
-.clear-button {
-  width: 100%;
-  margin-top: 10px;
-}
-
-.small-text {
-  font-size: 14px;
+table tr td {
+  width: calc(100% / 4);
 }
 </style>
